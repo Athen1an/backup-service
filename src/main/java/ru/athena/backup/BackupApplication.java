ec88453e475738f16b9d4e7b672997aff5be374f
dikii <dikii@vtb.ru>
2022-06-21 17:27:32 +0300

    BackupProperties init

--- src/main/java/ru/athena/backup/BackupApplication.java
+++ src/main/java/ru/athena/backup/BackupApplication.java
@@ -1,7 +1,12 @@
 package ru.athena.backup;
 
+import ru.athena.backup.configuration.BackupProperties;
+
 public class BackupApplication {
 
     public static void main(String[] args) {
+        BackupProperties backupProperties = BackupProperties.init();
+
+        System.out.println(backupProperties.getProperties());
     }
 }
--- src/main/java/ru/athena/backup/configuration/BackupProperties.java
+++ src/main/java/ru/athena/backup/configuration/BackupProperties.java
@@ -1,12 +1,31 @@
 package ru.athena.backup.configuration;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class BackupProperties {
 
+    private static final String CONFIG_FILE_NAME = "backup.config";
+
     private final Properties properties;
 
-    public BackupProperties(Properties properties) {
+    private BackupProperties(Properties properties) {
         this.properties = properties;
+    }
+
+    public static BackupProperties init() {
+        Properties properties = new Properties();
+        try {
+            FileReader fileReader = new FileReader(CONFIG_FILE_NAME);
+            properties.load(fileReader);
+        } catch (IOException e) {
+            System.out.println("Catch IOException: " + e.getMessage());
+        }
+        return new BackupProperties(properties);
+    }
+
+    public Properties getProperties() {
+        return properties;
     }
 }
