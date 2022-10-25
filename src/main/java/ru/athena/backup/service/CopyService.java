36ea3b13722d49bf8c3227d60504d284764be643
dikii <dikii@vtb.ru>
2022-10-25 13:15:50 +0300

    Adjust copy service

--- src/main/java/ru/athena/backup/service/CopyService.java
+++ src/main/java/ru/athena/backup/service/CopyService.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,29 +17,40 @@ public class CopyService {
     public void copy(Path sourceDirectory, Path targetDirectory) {
         File source = sourceDirectory.toFile();
         if (source.exists() && source.isDirectory()) {
-            if (!targetDirectory.toFile().exists()) {
-                try {
-                    Files.createDirectory(targetDirectory);
-                } catch (IOException e) {
-                    logger.log(Level.SEVERE, "Error occurred while create target directory.", e);
-                    throw new RuntimeException(e.getMessage());
-                }
-            }
-            File[] files = source.listFiles();
-            assert files != null;
+            createDirectory(targetDirectory);
+
+            File[] files = Objects.requireNonNull(source.listFiles());
             for (File file : files) {
-                if (!file.isDirectory() && !file.isHidden()) {
-                    try {
-                        Files.copy(file.toPath(), Paths.get(targetDirectory.toString(), file.getName()), StandardCopyOption.REPLACE_EXISTING);
-                    } catch (IOException e) {
-                        logger.log(Level.SEVERE, "Error occurred while copy file.", e);
-                        throw new RuntimeException(e.getMessage());
-                    }
-                }
+                copyFile(targetDirectory, file);
             }
             logger.log(Level.INFO, "Backup finished.");
         } else {
             logger.log(Level.WARNING, "Source directory does not exist, backup didn't take place.");
+        }
+    }
+
+    private void copyFile(Path targetDirectory, File file) {
+        if (file.isDirectory() || file.isHidden()) {
+            Object[] loggingParams = {file.getName(), file.isHidden(), file.isDirectory()};
+            logger.log(Level.FINE, "File {0} wasn't copy because it's hidden={1} or directory={2}", loggingParams);
+        } else {
+            try {
+                Files.copy(file.toPath(), Paths.get(targetDirectory.toString(), file.getName()), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Error occurred while copy file.", e);
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+
+    private void createDirectory(Path targetDirectory) {
+        if (targetDirectory.toFile().exists()) return;
+
+        try {
+            Files.createDirectory(targetDirectory);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error occurred while create target directory.", e);
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
